d97e05487b698ba3f126743c7145112de98d02a9
Luis Chodiman <lechodiman@uc.cl>
2024-06-03 22:15:27 -0400

    feat: Handle /echo path in main.ts
    The code changes in `main.ts` add functionality to handle requests to the `/echo` path. It extracts the string after `/echo/` and sends it back as the response body.

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,45 @@
 import * as net from 'net';
 
+function createResponse({
+  body,
+  headers,
+  statusCode,
+}: {
+  headers: {
+    [key: string]: string;
+  };
+  statusCode: string;
+  body: string;
+}) {
+  const responseHeaders = Object.entries(headers)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join('\r\n');
+
+  return `HTTP/1.1 ${statusCode}\r\n${responseHeaders}\r\n\r\n${body}`;
+}
+
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const requestLine = data.toString().split('\r\n')[0];
     const [method, path, version] = requestLine.split(' ');
+
     console.log([method, path, version]);
+
+    if (path.includes('/echo')) {
+      const echoStr = path.split('/echo/')[1];
+      console.log(echoStr);
+      socket.write(
+        createResponse({
+          headers: {
+            'Content-Type': 'text/plain',
+            'Content-Length': echoStr.length.toString(),
+          },
+          statusCode: '200 OK',
+          body: echoStr,
+        })
+      );
+      socket.end();
+    }
 
     if (path !== '/') {
       socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
