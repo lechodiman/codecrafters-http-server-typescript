1b8acee3b42f5610d542ca633537ca03f8a4914f
Luis Chodiman <lechodiman@uc.cl>
2024-06-03 22:30:17 -0400

    feat: Handle /echo and /user-agent paths in main.ts
    The code changes in `main.ts` add functionality to handle requests to the `/echo` and `/user-agent` paths. It extracts the string after `/echo/` and sends it back as the response body for `/echo` requests. For `/user-agent` requests, it retrieves the User-Agent header from the request and sends it back as the response body.

--- app/main.ts
+++ app/main.ts
@@ -1,29 +1,27 @@
 import * as net from 'net';
 
-function createResponse({
-  body,
-  headers,
-  statusCode,
-}: {
-  headers: {
-    [key: string]: string;
-  };
-  statusCode: string;
-  body: string;
-}) {
-  const responseHeaders = Object.entries(headers)
-    .map(([key, value]) => `${key}: ${value}`)
-    .join('\r\n');
-
-  return `HTTP/1.1 ${statusCode}\r\n${responseHeaders}\r\n\r\n${body}`;
-}
-
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const requestLine = data.toString().split('\r\n')[0];
+    const request = data.toString().split('\r\n');
+    const headers = getRequestHeaders(request);
+    const requestLine = request[0];
     const [method, path, version] = requestLine.split(' ');
 
-    console.log([method, path, version]);
+    if (path.includes('/user-agent')) {
+      const userAgent = headers['User-Agent'];
+      console.log('🚀 ~ socket.on ~ userAgent:', userAgent);
+      socket.write(
+        createResponse({
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+          statusCode: '200 OK',
+          body: userAgent,
+        })
+      );
+      socket.end();
+      return;
+    }
 
     if (path.includes('/echo')) {
       const echoStr = path.split('/echo/')[1];
@@ -32,30 +30,60 @@ const server = net.createServer((socket) => {
         createResponse({
           headers: {
             'Content-Type': 'text/plain',
-            'Content-Length': echoStr.length.toString(),
           },
           statusCode: '200 OK',
           body: echoStr,
         })
       );
       socket.end();
+      return;
     }
 
     if (path !== '/') {
-      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+      socket.write(createResponse({ statusCode: '404 Not Found' }));
       socket.end();
       return;
     }
 
-    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    socket.write(createResponse({ statusCode: '200 OK' }));
     socket.end();
   });
 });
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log('Logs from your program will appear here!');
-
-// Uncomment this to pass the first stage
 server.listen(4221, 'localhost', () => {
   console.log('Server is running on port 4221');
 });
+
+function createResponse({
+  body,
+  headers = {},
+  statusCode,
+}: {
+  headers?: {
+    [key: string]: string;
+  };
+  statusCode: string;
+  body?: string;
+}) {
+  const headersArray = Object.entries(headers).map(([key, value]) => `${key}: ${value}`);
+
+  if (body) {
+    headersArray.push('Content-Length: ' + body.length.toString());
+  }
+
+  const responseHeaders = headersArray.join('\r\n');
+
+  return `HTTP/1.1 ${statusCode}\r\n${responseHeaders}\r\n\r\n${body}`;
+}
+
+function getRequestHeaders(request: string[]) {
+  const headers = request.slice(1, -2); // last two elements are empty strings
+  const headersObject: { [key: string]: string } = {};
+
+  headers.forEach((header) => {
+    const [key, value] = header.split(': ');
+    headersObject[key] = value;
+  });
+
+  return headersObject;
+}
