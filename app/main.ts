50b8ae34e6f24c9e333ece3b5d6fc0dad1b3a0cb
Luis Chodiman <lechodiman@uc.cl>
2024-06-03 22:06:16 -0400

    Refactor main.ts to handle HTTP requests and improve code structure

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,18 @@ import * as net from 'net';
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
+    const requestLine = data.toString().split('\r\n')[0];
+    const [method, path, version] = requestLine.split(' ');
+    console.log([method, path, version]);
+
+    if (path !== '/') {
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+      socket.end();
+      return;
+    }
+
     socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    socket.end();
   });
 });
 
