5d2aa10cc406443c1c5120d4238c0a9725d6df24
Luis Chodiman <lechodiman@uc.cl>
2024-06-03 21:53:10 -0400

    feat: Add HTTP response to socket data event in main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,15 @@
 import * as net from 'net';
 
 const server = net.createServer((socket) => {
-  socket.end();
+  socket.on('data', (data) => {
+    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+  });
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
-
 server.listen(4221, 'localhost', () => {
   console.log('Server is running on port 4221');
 });
